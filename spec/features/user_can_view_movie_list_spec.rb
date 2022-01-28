cd1d1a924c27efa3c7e54722e740c41fb7d0efe8
Mark McLeod <markmcleod45@gmail.com>
2022-01-28 13:33:32 +0000

    Clear test database before tests run

--- spec/features/user_can_view_movie_list_spec.rb
+++ spec/features/user_can_view_movie_list_spec.rb
@@ -1,5 +1,12 @@
 feature 'View movie list' do
   scenario 'user can view a list of movies' do
+    #Connect to the test database
+    connection = PG.connect(dbname: 'movie_manager_test')
+    # Add data to the test database
+    connection.exec("INSERT INTO movies (title) VALUES ('The Godfather');")
+    connection.exec("INSERT INTO movies (title) VALUES ('Scarface');")
+    connection.exec("INSERT INTO movies (title) VALUES ('Goodfellas');")
+    
     visit "/"
     click_link "See Movies"
     expect(page).to have_content("Movies List")
