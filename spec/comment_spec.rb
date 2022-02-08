a9c4c0808498c76f2e32aaf48d9ee464fedfa703
Mark McLeod <markmcleod45@gmail.com>
2022-02-08 14:09:42 +0000

    Refactor unit test

--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -7,14 +7,13 @@ describe Comment do
       movie = Movie.create(title: 'Made up movie title')
 
       comment = Comment.create(text: 'This is made up comment text', movie_id: movie.id)
-      Comment.create(text: 'This is another made up comment text', movie_id: movie.id)
       test_connection = PG.connect(dbname: 'movie_manager_test')
       test_result = test_connection.exec_params("SELECT * FROM comments WHERE movie_id = #{movie.id};")
 
-      # expect(test_result[0]).to be_a Comment
-      expect(test_result[0]['id']).to eq comment.id
-      expect(test_result[0]['text']).to eq comment.text
-      expect(test_result[0]['movie_id']).to eq movie.id
+      expect(comment).to be_a Comment
+      expect(comment.id).to eq test_result[0]['id']
+      expect(comment.text).to eq 'This is made up comment text'
+      expect(comment.movie_id).to eq movie.id
     end
   end
 
